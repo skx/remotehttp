df4ddcb5cb432451c17122db58d6065ba6ba7a4f
Steve Kemp <steve@steve.org.uk>
2020-03-12 18:01:40 +0200

    Added simple test-cases

---
+++ remotehttp_test.go
@@ -0,0 +1,42 @@
+package remotehttp
+
+import (
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestLocalURLs(t *testing.T) {
+
+	// Local resources we should never fetch
+	tests := []string{"http://localhost/",
+		"http://127.0.0.1/server-status",
+		"https://localhost/",
+		"https://127.0.127.127/",
+		"http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key",
+		"http://[fe80::1]:6379/",
+	}
+
+	transport := Transport
+
+	var netClient = &http.Client{
+		Transport: transport,
+		Timeout:   5 * time.Second,
+	}
+
+	for _, url := range tests {
+
+		// Prepare
+		req, err := http.NewRequest("GET", url, nil)
+
+		if err != nil {
+			t.Fatalf("Unexpected error requesting %s %s", url, err.Error())
+		}
+
+		// Fetch
+		_, err = netClient.Do(req)
+		if err == nil {
+			t.Fatalf("Expected error requesting %s - expected to be denied", url)
+		}
+	}
+}
